626a95e495a6a64e78c91451f435ba6fbee8f527
kiquetal <kiquetal@gmail.com>
2018-01-07 23:17:31 -0300

    Testing curry and partial functions

---
+++ src/main/java/Functions1.java
@@ -0,0 +1,42 @@
+public class Functions1 {
+
+
+
+public static void main(String [] args)
+{
+
+    Function<Integer,Integer>doubleNumber =args1 -> args1*2;
+    Function<Integer,Integer>tripleNumber =args1 -> args1*3;
+
+    Functions1 f1=new Functions1();
+    System.out.println(combine(doubleNumber,tripleNumber).apply(5));
+    System.out.print(f1.add.apply(4).apply(3));
+
+
+}
+
+
+private static Function<Integer,Integer> combine(Function<Integer, Integer> f1, Function<Integer, Integer> f2)
+{
+    return args -> f1.apply(f2.apply((args)));
+
+
+
+}
+
+    private interface BinaryOperator extends
+            Function<Integer, Function<Integer, Integer>> {
+
+    }
+
+    private BinaryOperator       add = x -> y -> x + y;
+    private BinaryOperator mult = x -> y -> x * y;
+
+    interface Function <T,U>
+    {
+        U apply (T args);
+
+
+    }
+
+}
